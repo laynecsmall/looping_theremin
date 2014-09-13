641f44815f5703467dd7176044c42a4758d55648
Layne Small <laynecsmall@gmail.com>
2014-09-13 14:50:56 +1200

    function test for switching pins via binary

---
+++ arduino/binary_switching/binary_switching/binary_switching.ino
@@ -0,0 +1,14 @@
+int binary_pins(int num, int pin){
+ return (num >> pin) & 1;
+}
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  for (int i=0;i<8;i++){
+    Serial.println(binary_pins(5,i));
+  }
+  Serial.println("\n");
+  delay(5000);
+}
